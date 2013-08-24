be835200bc55c7a109750bf866abae4f3b5dcf27
sekaiya <sekai.kobayashi@gmail.com>
2013-08-24 21:06:21 +0900

    refactoring option-methods

--- lib/todo_samp/command/options.rb
+++ lib/todo_samp/command/options.rb
@@ -7,23 +7,8 @@ module TodoSamp
 			def self.parse!(argv)
 				options = {}
 				
-				#サブコマンドの処理をする際に、未定義のkeyを指定されたら例外を発生させる
-				sub_cps = Hash.new do |hash, key|
-					raise ArgumentError, "'#{key}' is not todo sub command."
-				end
-				
-				sub_cps['create'] = OptionParser.new do |opt|
-					opt.on('-n VAL', '--name=VAL', 'task name') {|v| options[:name] = v}
-					opt.on('-c VAL', '--content=VAL', 'task content') {|v| options[:name] = v}
-				end
-				
-				cp = OptionParser.new do |opt|
-					opt.on_head('-v','--version', 'show ver') do |v|
-						opt.version = TodoSamp::VERSION
-						puts opt.ver + "dayo"
-						exit
-					end
-				end
+				sub_cps = create_sub_cp(options)
+				cp = create_cp
 				
 				begin
 					cp.order!(argv)
@@ -33,6 +18,29 @@ module TodoSamp
 					abort e.message
 				end
 				options
+			end
+			
+			def self.create_sub_cp(options)
+				#サブコマンドの処理をする際に、未定義のkeyを指定されたら例外を発生させる
+				sub_cps = Hash.new do |hash, key|
+					raise ArgumentError, "'#{key}' is not todo sub command."
+				end
+				
+				sub_cps['create'] = OptionParser.new do |opt|
+					opt.on('-n VAL', '--name=VAL', 'task name') {|v| options[:name] = v}
+					opt.on('-c VAL', '--content=VAL', 'task content') {|v| options[:content] = v}
+				end
+				sub_cps
+			end
+			
+			def self.create_cp
+				OptionParser.new do |opt|
+					opt.on_head('-v','--version', 'show ver') do |v|
+						opt.version = TodoSamp::VERSION
+						puts opt.ver + "dayo"
+						exit
+					end
+				end
 			end
 			
 		end
